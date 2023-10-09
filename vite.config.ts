95359b6027f46f6369d1424250f9026822597a5f
Nhat Quang <nhatquang.fit@gmail.com>
2023-10-09 22:26:11 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build', // Thư mục output cho production build
+    outDir: 'dist', // Thư mục output cho production build
   },
   plugins: [
     react(),
