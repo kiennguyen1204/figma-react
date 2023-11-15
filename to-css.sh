1887164bb58128786e8505dca0df0ad8a8928155
DESKTOP-6ETSHUK\GOSU <nhatquang1397@gmail.com>
2023-11-15 18:29:06 +0700

    .

---
+++ to-css.sh
@@ -0,0 +1,1 @@
+find src -type f -name '*.css' -exec rm -f {} + && find src -type f -name '*.scss' -exec sh -c 'scss_file="{}"; css_file="${scss_file%.scss}.css"; echo "Compiling $scss_file to $css_file"; ./node_modules/.bin/node-sass "$scss_file" "$css_file"' \;
